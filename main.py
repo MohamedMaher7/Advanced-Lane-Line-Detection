6274369351e1a091faf248c8c47ddd327466018d
nesmaabdelaziz <73910446+nesmaabdelaziz@users.noreply.github.com>
2022-04-19 00:35:43 +0200

    Camera Calibration

---
+++ main.py
@@ -0,0 +1,39 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+import glob
+import matplotlib.image as mpimg
+from moviepy.editor import VideoFileClip
+from collections import deque
+
+
+def camera_calibration():
+    global mtx,dist
+    # Prepare object points
+    obj_pts = np.zeros((6 * 9, 3), np.float32)
+    obj_pts[:, :2] = np.mgrid[0:9, 0:6].T.reshape(-1, 2)
+
+    # Stores all object points & img points from all images
+    objpoints = []
+    imgpoints = []
+
+    # Get directory for all calibration images
+    images = glob.glob('./camera_cal/*.jpg')
+    img = None
+
+    for indx, fname in enumerate(images):
+        img = cv2.imread(fname)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # MUST be 8-bit grayscale or color image
+        ret, corners = cv2.findChessboardCorners(gray, (9, 6), None)
+
+        if ret == True:
+            objpoints.append(obj_pts)
+            imgpoints.append(corners)
+
+    # Get Image Size
+    img_size = (img.shape[1], img.shape[0])
+
+    # Calibrate camera
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size, None, None)
